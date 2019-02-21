3590bbf4e23a7241f08a97ca14d5f4834d3411df
smriti241 <smriti.agarwal_da17@gla.ac.in>
2019-02-21 08:23:10 +0530

    Add node class

---
+++ src/Search/ARList.java
@@ -0,0 +1,18 @@
+package Search;
+import java.util.ArrayList;
+public class ARList
+{
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> list=new ArrayList<Integer>();
+        for (int i = 0; i <10;i++) {
+            list.add(0,i);
+        }
+        //list.add(42);
+        System.out.println(list);
+        /*list.add()
+        * list.remove()
+        * list.IndexOf()
+        * list.set()*/
+    }
+}
--- src/Search/BinarySearch.java
+++ src/Search/BinarySearch.java
@@ -1,21 +1,80 @@
 package Search;
 
+import java.util.Scanner;
+
+class Car
+{
+    private String name;
+    private int price;
+
+    public String getName() {
+        return name;
+    }
+    public int getPrice() {
+        return price;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    @Override
+    public String toString() {
+        return "Car{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
+
 public class BinarySearch
 {
+    private static void insert(Car[] car)
+    {
+        for (int i = 0; i < car.length; i++)
+        {
+            car[i]=new Car();
+            car[i].setName("Car"+i);
+            car[i].setPrice(i*100);
+        }
+    }
     public static void main(String[] args)
     {
-        int size=10;
-        int[] array = new int[size];
-        for (int i = 0; i < array.length; i++)
-        {
-            array[i]=(i+1)*100;
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        Car[] array = new Car[size];
+        insert(array);
 
-        }
-        int value=200;
+        /*for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i].toString());
+        }*/
+        int value=sc.nextInt();
+        System.out.println("Position: "+Binary(array,value));
+
     }
-    public static int BinarySearch(int[] array,int value)
+    public static int Binary(Car[] array,int value)
     {
         int pos=-1;
         int beg=0,end=array.length-1,mid;
+        while(beg<=end)
+        {
+            mid=(beg+end)/2;
+            if(array[mid].getPrice()==value)
+            {
+                pos=mid;
+                break;
+            }
+            else if(array[mid].getPrice()>value)
+            {
+                end=mid-1;
+            }
+            else
+            {
+                beg=mid+1;
+            }
+        }
+        return pos;
     }
 }
